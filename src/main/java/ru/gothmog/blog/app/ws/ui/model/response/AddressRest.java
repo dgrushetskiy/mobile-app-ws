21598898c19768576a16503fafe20f5d9c35947e
Дмитрий Грушецкий <d_grushetskiy@dev-city.local>
2019-12-18 17:49:00 +0300

    adding HATEOAS support to project

--- src/main/java/ru/gothmog/blog/app/ws/ui/model/response/AddressRest.java
+++ src/main/java/ru/gothmog/blog/app/ws/ui/model/response/AddressRest.java
@@ -2,10 +2,11 @@ package ru.gothmog.blog.app.ws.ui.model.response;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
 
 @Getter
 @Setter
-public class AddressRest {
+public class AddressRest extends RepresentationModel {
     private String addressId;
     private String city;
     private String country;
